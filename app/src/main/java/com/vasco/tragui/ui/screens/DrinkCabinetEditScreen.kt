ab846b72c215e2c24ff2ee069c4ea1c824820ac1
Santiago Benedetti <sbenedetti@frba.utn.edu.ar>
2024-05-02 17:53:05 -0300

    added title and simple loader

--- app/src/main/java/com/vasco/tragui/ui/screens/DrinkCabinetEditScreen.kt
+++ app/src/main/java/com/vasco/tragui/ui/screens/DrinkCabinetEditScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -68,9 +69,7 @@ class DrinkCabinetEditScreen: Screen {
     @SuppressLint("MutableCollectionMutableState")
     @Composable
     override fun Content() {
-
         val logger = Logger.getLogger("VascoLogger")
-
 
         val context = LocalContext.current
         val scope = rememberCoroutineScope()
@@ -86,9 +85,8 @@ class DrinkCabinetEditScreen: Screen {
             mutableStateOf(emptyList<String>())
         }
 
-
-        var input by remember {
-            mutableStateOf("")
+        var loading by remember {
+            mutableStateOf(true)
         }
 
         LaunchedEffect(Unit) {
@@ -103,6 +101,7 @@ class DrinkCabinetEditScreen: Screen {
 //                        userBottles.value.
 //                        selectedBottles + currentBottle
 //                        selectedBottles
+                        loading = false
                     }
                 }
             }
@@ -113,87 +112,102 @@ class DrinkCabinetEditScreen: Screen {
                 .fillMaxSize()
                 .background(color = colorResource(id = R.color.primary_light))
         ) {
-            (if (userBottles != null) userBottles.value else selectedBottles.toString())?.let { Text(text = it, fontSize = 20.sp) }
+//            (if (userBottles != null) userBottles.value else selectedBottles.toString())?.let { Text(text = it, fontSize = 20.sp) }
+            Text (
+                text = "What do you have at hand?",
+                textAlign = TextAlign.Center,
+                lineHeight = 50.sp,
+                fontFamily = pixelfyFontFamily,
+                fontSize = 40.sp,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp, vertical = 20.dp)
+            )
 
             Box (modifier = Modifier.fillMaxSize()) {
+                if (loading)
+                    Box (modifier = Modifier.fillMaxWidth().align(Alignment.Center)) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
+                else {
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(150.dp),
+                        modifier = Modifier.fillMaxSize(),
+                        contentPadding = PaddingValues(25.dp),
+                        verticalArrangement = Arrangement.spacedBy(30.dp),
+                        horizontalArrangement = Arrangement.spacedBy(30.dp)
+                    ) {
+                        bottles?.size?.let {
+                            items(it) { bottleIndex ->
 
-
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(150.dp),
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(25.dp),
-                verticalArrangement = Arrangement.spacedBy(30.dp),
-                horizontalArrangement = Arrangement.spacedBy(30.dp)
-            ) {
-                bottles?.size?.let {
-                    items(it) { bottleIndex ->
-
-                                    Card(
-                                        border = BorderStroke(4.dp, Color.Black),
-                                        shape = RectangleShape,
-                                        colors = CardColors(
-                                            containerColor = Color.White,
-                                            contentColor = Color.Black,
-                                            disabledContentColor = Color.Gray,
-                                            disabledContainerColor = Color.Black,
-                                        ),
-                                        modifier = Modifier
-                                            .height(200.dp)
-                                            .shadowsPlus(
-                                                type = ShadowsPlusType.SoftLayer,
-                                                color = colorResource(id = R.color.black).copy(alpha = 0.25f),
-                                                spread = (-5).dp,
-                                                offset = DpOffset(9.dp, 9.dp),
-                                                radius = 0.dp
-                                            )
-                                            .background(Color.White)
-                                    ) {
-                                        Box (modifier = Modifier
-                                            .fillMaxHeight()
-                                            .padding(vertical = 10.dp)) {
-                                            AsyncImage(
-                                                model = "https://www.thecocktaildb.com/images/ingredients/${bottles!!.get(bottleIndex)}-Medium.png",
-                                                contentDescription = bottles!!.get(bottleIndex),
-                                                modifier = Modifier
-                                                    .fillMaxWidth()
-                                                    .align(Alignment.TopCenter)
-                                            )
-                                            Text(
-                                                text = bottles!!.get(bottleIndex),
-                                                color = Color.Black,
-                                                fontFamily = pixelfyFontFamily,
-                                                fontSize = 18.sp,
-                                                textAlign = TextAlign.Center,
-                                                modifier = Modifier
-                                                    .align(Alignment.BottomCenter)
-                                                    .fillMaxWidth()
-                                                    .padding(horizontal = 1.dp)
-                                            )
-                                        }
+                                Card(
+                                    border = BorderStroke(4.dp, Color.Black),
+                                    shape = RectangleShape,
+                                    colors = CardColors(
+                                        containerColor = Color.White,
+                                        contentColor = Color.Black,
+                                        disabledContentColor = Color.Gray,
+                                        disabledContainerColor = Color.Black,
+                                    ),
+                                    modifier = Modifier
+                                        .height(200.dp)
+                                        .shadowsPlus(
+                                            type = ShadowsPlusType.SoftLayer,
+                                            color = colorResource(id = R.color.black).copy(alpha = 0.25f),
+                                            spread = (-5).dp,
+                                            offset = DpOffset(9.dp, 9.dp),
+                                            radius = 0.dp
+                                        )
+                                        .background(Color.White)
+                                ) {
+                                    Box (modifier = Modifier
+                                        .fillMaxHeight()
+                                        .padding(vertical = 10.dp)) {
+                                        AsyncImage(
+                                            model = "https://www.thecocktaildb.com/images/ingredients/${bottles!!.get(bottleIndex)}-Medium.png",
+                                            contentDescription = bottles!!.get(bottleIndex),
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .align(Alignment.TopCenter)
+                                        )
+                                        Text(
+                                            text = bottles!!.get(bottleIndex),
+                                            color = Color.Black,
+                                            fontFamily = pixelfyFontFamily,
+                                            fontSize = 18.sp,
+                                            textAlign = TextAlign.Center,
+                                            modifier = Modifier
+                                                .align(Alignment.BottomCenter)
+                                                .fillMaxWidth()
+                                                .padding(horizontal = 1.dp)
+                                        )
                                     }
-                        IconButton(onClick = {
-                            val currentBottle = bottles!![bottleIndex]
-                            val updatedSelectedBottles =
-                                if (selectedBottles.contains(currentBottle)) {
-                                    selectedBottles.filter { it != currentBottle }
-                                } else {
-                                    selectedBottles + currentBottle
                                 }
+                                IconButton(onClick = {
+                                    val currentBottle = bottles!![bottleIndex]
+                                    val updatedSelectedBottles =
+                                        if (selectedBottles.contains(currentBottle)) {
+                                            selectedBottles.filter { it != currentBottle }
+                                        } else {
+                                            selectedBottles + currentBottle
+                                        }
 
-                            selectedBottles = updatedSelectedBottles
-                            logger.info(selectedBottles.toString())
-                        }, modifier = Modifier
-                            .offset(x = 73.dp, y = (-15).dp)
+                                    selectedBottles = updatedSelectedBottles
+                                    logger.info(selectedBottles.toString())
+                                }, modifier = Modifier
+                                    .offset(x = 73.dp, y = (-15).dp)
 //                            .background(Color.Black) TODO
-                        ) {
-                            Image(
-                                painter = painterResource(id = if (selectedBottles.contains(bottles!!.get(bottleIndex))) R.drawable.remove else R.drawable.add),
-                                contentDescription = "add ${bottles!!.get(bottleIndex)}",
-                            )
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = if (selectedBottles.contains(bottles!!.get(bottleIndex))) R.drawable.remove else R.drawable.add),
+                                        contentDescription = "add ${bottles!!.get(bottleIndex)}",
+                                    )
+                                }
+                            }
                         }
                     }
+
                 }
-            }
+
+
             Button(onClick = {
                 scope.launch {
                     dataStore.saveBottles(selectedBottles.toString())

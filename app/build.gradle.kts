212b8d5e7b5d9ff5c3831c2adb35521fc0696efe
santiagobenedetti <sbenedetti@frba.utn.edu.ar>
2024-05-03 15:26:52 -0300

    removed old dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,6 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // para el gif
-    implementation ("io.coil-kt:coil-compose:2.1.0")
     implementation ("io.coil-kt:coil-gif:2.1.0")
 
     // Coroutines

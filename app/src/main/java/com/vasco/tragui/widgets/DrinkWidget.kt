3b88fd5cc457c72042468d8481dcfc77f2f5dbc3
santiagobenedetti <sbenedetti@frba.utn.edu.ar>
2024-06-03 20:33:33 -0300

    simple working widget

--- app/src/main/java/com/vasco/tragui/widgets/DrinkWidget.kt
+++ app/src/main/java/com/vasco/tragui/widgets/DrinkWidget.kt
@@ -1,11 +1,24 @@
+package com.vasco.tragui.widgets
+
+import android.annotation.SuppressLint
 import android.content.Context
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.text.FontFamily
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+import com.vasco.tragui.R
 
 class DrinkWidgetReceiver: GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = DrinkWidget()
@@ -15,6 +28,7 @@ class DrinkWidget : GlanceAppWidget() {
 
     override val sizeMode: SizeMode = SizeMode.Exact
 
+    @SuppressLint("RestrictedApi")
     override suspend fun provideGlance(context: Context, id: GlanceId) {
 
         // In this method, load data needed to render the AppWidget.
@@ -24,7 +38,21 @@ class DrinkWidget : GlanceAppWidget() {
         provideContent {
             // create your AppWidget here
             GlanceTheme {
-                Text(text = "Where to?")
+                Column(
+                    modifier = GlanceModifier
+                        .fillMaxSize()
+                        .background(R.color.primary_dark),
+                    verticalAlignment = Alignment.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Try this new drink",
+                        style = TextStyle(
+                            color = ColorProvider(R.color.white),
+                            fontSize = 32.sp,
+                        )
+                    )
+                }
             }
         }
     }
